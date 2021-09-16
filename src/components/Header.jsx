b8ac146096b82dd47e09eab8dfae91b9b21ebece
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-09-16 19:19:37 +0200

    feat: add styles to the project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
     </header>
   );
@@ -15,5 +15,11 @@ Header.defaultProps = {
 Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
+
+// CSS in JS
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
 
 export default Header;
