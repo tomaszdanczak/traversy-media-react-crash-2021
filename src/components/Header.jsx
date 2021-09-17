13e1623422390a04bcd2bbd662eb3ee887b9228d
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-09-17 14:05:52 +0200

    feat: add routing in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
 const Header = ({ title, showAdd, onShowAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClick={onShowAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onShowAdd}
+        />
+      )}
     </header>
   );
 };
